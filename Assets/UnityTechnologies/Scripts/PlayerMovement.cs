86fe66024bea2ad9d087e2cd62911313f44dbb0a
dbin7800 <oi0720610@naver.com>
2024-04-22 17:14:02 +0900

    시험 제출

--- Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -19,9 +19,9 @@ public class PlayerMovement : MonoBehaviour
     public float turnSpeed = 20f;
 
     // 문제 1에서 사용해야 할 변수들
-    private Animator animator;
+    private Animator animator ;
     private Rigidbody rb;
-    private Vector3 movement;
+    private Vector3 movement ;
     private Quaternion rotation = Quaternion.identity;
 
     private void Start()
@@ -35,8 +35,8 @@ public class PlayerMovement : MonoBehaviour
     {
         // 문제 2) Input.GetAxis를 사용하여 존 레몬이 움직일 수 있게 좌표를 입력하세요.
         // 0f 대신 정답을 채워넣으면 작동합니다. (각 변수당 5점, 총 10점)
-        float horizontal = 0f; // 이 변수와
-        float vertical = 0f; // 이 변수를 사용해야 합니다.
+        float horizontal = Input.GetAxis("horizontal"); // 이 변수와
+        float vertical = Input.GetAxis(" vertical"); // 이 변수를 사용해야 합니다.
         
         #region 건드리면 작동 안 됨
         movement.Set(horizontal, 0f, vertical);
@@ -46,15 +46,18 @@ public class PlayerMovement : MonoBehaviour
         // 문제 3) isWalking은 hasHorizontalInput과 hasVerticalInput중 하나만 입력되어도 true(참)가 되어야 합니다.
         // false를 지우고 제대로 된 명령줄을 완성해 주세요. (10점)
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);        
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
 
-        bool isWalking = false;
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
 
         // 문제 4) isWalking 변수를 Animator의 IsWalking 파라미터에 적용하세요. (10점)
+        //넘호 어려워요ㅜㅜ
 
+        Animator. Setbool( IsWalking, "iswalking" );  //대충이거였는데,,,
 
-        // 문제 5) 원하는 방향으로 이동할 수 있는 Vector3값을 만드는 명령줄을 Vector3.zero를 지우고 완성해 주세요. (10점)
-        Vector3 desiredFoward = Vector3.zero;
+      // 문제 5) 원하는 방향으로 이동할 수 있는 Vector3값을 만드는 명령줄을 Vector3.zero를 지우고 완성해 주세요. (10점)
+      Vector3 desiredFoward = 
         rotation = Quaternion.LookRotation(desiredFoward);
 
     }
